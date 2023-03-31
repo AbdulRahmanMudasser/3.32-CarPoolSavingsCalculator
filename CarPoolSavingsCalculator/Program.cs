4bef0e91bcf60d07156131366b29191b85e44383
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-03-31 10:43:55 +0500

    half way

--- CarPoolSavingsCalculator/Program.cs
+++ CarPoolSavingsCalculator/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class C3Q32
+{
+    static void Main(string[] args)
+    {
+        // declaring variables for user to input information
+        int totalMilesPerDay;
+        int costPerGallon;
+        int milesPerGallon;
+        int parkingFeePerDay;
+        int tollsPerDay;
+
+        // prompting user to get input
+        Console.WriteLine("Input Total Miles Driven Per Day:  ");
+        totalMilesPerDay = Co
+    }
+}
