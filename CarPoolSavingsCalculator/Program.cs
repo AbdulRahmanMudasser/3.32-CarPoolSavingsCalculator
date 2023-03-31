2aadef8f937a64ecf153aa65c51f5e9cf9f8e942
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-03-31 11:29:14 +0500

    taking input

--- CarPoolSavingsCalculator/Program.cs
+++ CarPoolSavingsCalculator/Program.cs
@@ -9,8 +9,24 @@
         int parkingFeePerDay;
         int tollsPerDay;
 
-        // prompting user to get input
+        // prompting user to get input for totalMilesPerDay
         Console.WriteLine("Input Total Miles Driven Per Day:  ");
-        totalMilesPerDay = Co
+        totalMilesPerDay = Convert.ToInt32(Console.ReadLine());
+
+        // prompting user to get input for costPerGallon
+        Console.WriteLine("Input Cost Per Gallon Of Gasoline (in cents):  ");
+        costPerGallon = Convert.ToInt32(Console.ReadLine());
+
+        // prompting user to get input for milesPerGallon
+        Console.WriteLine("Input Average Mile Per Gallon:  ");
+        milesPerGallon = Convert.ToInt32(Console.ReadLine());
+
+        // prompting user to get input for parkingFeePerDay
+        Console.WriteLine("Input Parking Fee Per Day (in cents):  ");
+        parkingFeePerDay = Convert.ToInt32(Console.ReadLine());
+
+        // prompting user to get input for tollsPerDay
+        Console.WriteLine("Input Tolls Per Day (in cents):  ");
+        tollsPerDay = Convert.ToInt32(Console.ReadLine());
     }
 }
