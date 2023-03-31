e76749988608d49982b3de57e95fd16276abfef3
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-03-31 11:43:23 +0500

    solution done

--- CarPoolSavingsCalculator/Program.cs
+++ CarPoolSavingsCalculator/Program.cs
@@ -9,24 +9,46 @@
         int parkingFeePerDay;
         int tollsPerDay;
 
+        // declaring variables for estimating cost
+        int gallonsUsed;
+        int fuelCost;
+        int pricePerDay;
+
         // prompting user to get input for totalMilesPerDay
-        Console.WriteLine("Input Total Miles Driven Per Day:  ");
+        Console.Write("Input Total Miles Driven Per Day:  ");
         totalMilesPerDay = Convert.ToInt32(Console.ReadLine());
 
         // prompting user to get input for costPerGallon
-        Console.WriteLine("Input Cost Per Gallon Of Gasoline (in cents):  ");
+        Console.Write("Input Cost Per Gallon Of Gasoline (in cents):  ");
         costPerGallon = Convert.ToInt32(Console.ReadLine());
 
         // prompting user to get input for milesPerGallon
-        Console.WriteLine("Input Average Mile Per Gallon:  ");
+        Console.Write("Input Average Mile Per Gallon:  ");
         milesPerGallon = Convert.ToInt32(Console.ReadLine());
 
         // prompting user to get input for parkingFeePerDay
-        Console.WriteLine("Input Parking Fee Per Day (in cents):  ");
+        Console.Write("Input Parking Fee Per Day (in cents):  ");
         parkingFeePerDay = Convert.ToInt32(Console.ReadLine());
 
         // prompting user to get input for tollsPerDay
-        Console.WriteLine("Input Tolls Per Day (in cents):  ");
+        Console.Write("Input Tolls Per Day (in cents):  ");
         tollsPerDay = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
+
+        // calculating how many fuel is used
+        gallonsUsed = totalMilesPerDay / milesPerGallon;
+
+        // calculating what is the cost of fuel consumed
+        fuelCost = gallonsUsed * costPerGallon;
+
+        // calculating total cost
+        pricePerDay = fuelCost + tollsPerDay + parkingFeePerDay;
+
+        // converting total price to dollars
+        pricePerDay /= 100;
+
+        // displaying cost
+        Console.WriteLine("Total Cost Per Day:  " + pricePerDay + "$");
     }
 }
